875f2b93c220ffea5dfcaa1ea452829af475d9d0
Olivier Sarrouy <olivier@pando.network>
2019-02-25 16:53:59 +0100

    Clean master branch before merge

--- contracts/bondingcurve/IBondingCurve.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.24;
-
-
-contract IBondingCurve {
-    function buy(uint256 _value) external payable;
-    function sell(uint256 _amount) external;
-}
--- contracts/collateralpool/ICollateralPool.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.24;
-
-
-contract ICollateralPool {
-    function deposit(address _token, uint256 _value) external payable;
-    function transfer(address _token, uint256 _value) external;
-    function execute(address _target, bytes _data) external;
-}
