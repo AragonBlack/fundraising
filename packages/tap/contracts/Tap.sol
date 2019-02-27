f76f6f59fc680fb43182a4d858e768afb489ec64
Olivier Sarrouy <olivier@pando.network>
2019-02-27 21:06:53 +0100

    Update tap system

--- packages/tap/contracts/Tap.sol
+++ packages/tap/contracts/Tap.sol
@@ -7,66 +7,74 @@ pragma solidity ^0.4.24;
 import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/common/EtherTokenConstant.sol";
 import "@aragon/os/contracts/common/IsContract.sol";
+import "@aragon/os/contracts/common/SafeERC20.sol";
+import "@aragon/os/contracts/lib/token/ERC20.sol";
+import "@aragon/os/contracts/lib/math/SafeMath.sol";
 
-import "@aragon/apps-finance/contracts/Finance.sol";
-
+import "@aragon/apps-vault/contracts/Vault.sol";
 import "@aragonblack/fundraising-pool/contracts/Pool.sol";
 
 
 contract Tap is EtherTokenConstant, IsContract, AragonApp {
-    bytes32 public constant ADD_TOKEN_ROLE = keccak256("ADD_TOKEN_ROLE");
-    bytes32 public constant REMOVE_TOKEN_ROLE = keccak256("REMOVE_TOKEN_ROLE");
+    using SafeERC20 for ERC20;
+    using SafeMath for uint256;
+
+    bytes32 public constant ADD_TOKEN_TAP_ROLE = keccak256("ADD_TOKEN_TAP_ROLE");
+    bytes32 public constant REMOVE_TOKEN_TAP_ROLE = keccak256("REMOVE_TOKEN_TAP_ROLE");
     bytes32 public constant UPDATE_TOKEN_TAP_ROLE = keccak256("UPDATE_TOKEN_TAP_ROLE");
     bytes32 public constant UPDATE_POOL_ROLE = keccak256("UPDATE_POOL_ROLE");
-    bytes32 public constant UPDATE_FINANCE_ROLE = keccak256("UPDATE_FINANCE_ROLE");
+    bytes32 public constant UPDATE_VAULT_ROLE = keccak256("UPDATE_VAULT_ROLE");
+    bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
 
     string private constant ERROR_POOL_NOT_CONTRACT = "TAP_POOL_NOT_CONTRACT";
-    string private constant ERROR_FINANCE_NOT_CONTRACT = "TAP_FINANCE_NOT_CONTRACT";
+    string private constant ERROR_VAULT_NOT_CONTRACT = "TAP_VAULT_NOT_CONTRACT";
     string private constant ERROR_TOKEN_NOT_ETH_OR_CONTRACT = "TAP_TOKEN_NOT_ETH_OR_CONTRACT";
-    string private constant ERROR_TOKEN_ALREADY_EXISTS = "TAP_TOKEN_ALREADY_EXISTS";
-    string private constant ERROR_TOKEN_DOES_NOT_EXIST = "TAP_TOKEN_DOES_NOT_EXIST";
+    string private constant ERROR_TOKEN_TAP_ALREADY_EXISTS = "TAP_TOKEN_TAP_ALREADY_EXISTS";
+    string private constant ERROR_TOKEN_TAP_DOES_NOT_EXIST = "TAP_TOKEN_TAP_DOES_NOT_EXIST";
     string private constant ERROR_TAP_RATE_ZERO = "TAP_TAP_RATE_ZERO";
+    string private constant ERROR_WITHDRAWAL_VALUE_ZERO = "TAP_WITHDRAWAL_VALUE_ZERO";
 
     Pool public pool;
-    Finance public finance;
+    Vault public vault;
 
     mapping (address => uint256) public taps;
     mapping (address => uint256) public lastWithdrawals;
 
-    event AddToken(address indexed token, uint256 tap);
-    event RemoveToken(address indexed token);
+    event AddTokenTap(address indexed token, uint256 tap);
+    event RemoveTokenTap(address indexed token);
     event UpdateTokenTap(address indexed token, uint256 tap);
     event UpdatePool(address pool);
-    event UpdateFinance(address finance);
+    event UpdateVault(address vault);
+    event Withdraw(address indexed token, uint256 value);
 
-    function initialize(Pool _pool, Finance _finance) onlyInit {
+    function initialize(Pool _pool, Vault _vault) public onlyInit {
         initialized();
 
         require(isContract(_pool), ERROR_POOL_NOT_CONTRACT);
-        require(isContract(_finance), ERROR_FINANCE_NOT_CONTRACT);
+        require(isContract(_vault), ERROR_VAULT_NOT_CONTRACT);
 
         pool = _pool;
-        finance = _finance;
+        vault = _vault;
     }
 
     /***** external function *****/
 
-    function addToken(address _token, uint256 _tap) external auth(ADD_TOKEN_ROLE) {
+    function addTokenTap(address _token, uint256 _tap) external auth(ADD_TOKEN_TAP_ROLE) {
         require(_token == ETH || isContract(_token), ERROR_TOKEN_NOT_ETH_OR_CONTRACT);
-        require(taps[_token] == uint256(0), ERROR_TOKEN_ALREADY_EXISTS);
+        require(taps[_token] == uint256(0), ERROR_TOKEN_TAP_ALREADY_EXISTS);
         require(_tap > 0, ERROR_TAP_RATE_ZERO);
 
-        _addToken(_token, _tap);
+        _addTokenTap(_token, _tap);
     }
 
-    function removeToken(address _token) external auth(REMOVE_TOKEN_ROLE) {
-        require(taps[_token] != uint256(0), ERROR_TOKEN_DOES_NOT_EXIST);
+    function removeTokenTap(address _token) external auth(REMOVE_TOKEN_TAP_ROLE) {
+        require(taps[_token] != uint256(0), ERROR_TOKEN_TAP_DOES_NOT_EXIST);
 
-        _removeToken(_token);
+        _removeTokenTap(_token);
     }
 
     function updateTokenTap(address _token, uint256 _tap) external auth(UPDATE_TOKEN_TAP_ROLE) {
-        require(taps[_token] != uint256(0), ERROR_TOKEN_DOES_NOT_EXIST);
+        require(taps[_token] != uint256(0), ERROR_TOKEN_TAP_DOES_NOT_EXIST);
         require(_tap > 0, ERROR_TAP_RATE_ZERO);
 
         _updateTokenTap(_token, _tap);
@@ -78,24 +86,50 @@ contract Tap is EtherTokenConstant, IsContract, AragonApp {
         _updatePool(_pool);
     }
 
-    function updateFinance(Finance _finance) external auth(UPDATE_FINANCE_ROLE) {
-        require(isContract(_finance), ERROR_FINANCE_NOT_CONTRACT);
+    function updateVault(Vault _vault) external auth(UPDATE_VAULT_ROLE) {
+        require(isContract(_vault), ERROR_VAULT_NOT_CONTRACT);
 
-        _updateFinance(_finance);
+        _updateVault(_vault);
+    }
+
+    function withdraw(address _token) external auth(WITHDRAW_ROLE) {
+        require(taps[_token] > 0, ERROR_TOKEN_TAP_DOES_NOT_EXIST);
+        uint256 value = getWithdrawalValue(_token);
+        require(value > 0, ERROR_WITHDRAWAL_VALUE_ZERO);
+
+        _withdraw(_token, value);
+    }
+
+    /***** public functions *****/
+
+    function poolBalance(address _token) public view isInitialized returns (uint256) {
+        if (_token == ETH) {
+            return address(pool).balance;
+        } else {
+            return ERC20(_token).staticBalanceOf(pool);
+        }
+    }
+
+    function getWithdrawalValue(address _token) public view isInitialized returns (uint256) {
+        uint256 balance = poolBalance(_token);
+        uint256 max = (now.sub(lastWithdrawals[_token])).mul(taps[_token]);
+        return max > balance ? balance : max;
     }
 
     /***** internal functions *****/
 
-    function _addToken(address _token, uint256 _tap) internal {
+    function _addTokenTap(address _token, uint256 _tap) internal {
         taps[_token] = _tap;
+        lastWithdrawals[_token] = now;
 
-        emit AddToken(_token, _tap);
+        emit AddTokenTap(_token, _tap);
     }
 
-    function _removeToken(address _token) internal {
+    function _removeTokenTap(address _token) internal {
         taps[_token] = uint256(0);
-
-        emit RemoveToken(_token);
+        // no need to re-initialize lastWithdrawals[_token] as it
+        // will be automatically updated if the token is re-added
+        emit RemoveTokenTap(_token);
     }
 
     function _updateTokenTap(address _token, uint256 _tap) internal {
@@ -104,21 +138,24 @@ contract Tap is EtherTokenConstant, IsContract, AragonApp {
         emit UpdateTokenTap(_token, _tap);
     }
 
-
-
     function _updatePool(Pool _pool) internal {
         pool = _pool;
 
         emit UpdatePool(address(_pool));
     }
 
-    function _updateFinance(Finance _finance) internal {
-        finance = _finance;
+    function _updateVault(Vault _vault) internal {
+        vault = _vault;
 
-        emit UpdateFinance(address(_finance));
+        emit UpdateVault(address(_vault));
     }
 
+    function _withdraw(address _token, uint256 _value) internal {
+        lastWithdrawals[_token] = now;
+        pool.transfer(_token, vault, _value); // Pool / Agent / Vault contacts transfer method already throws on error
 
+        emit Withdraw(_token, _value);
+    }
 
 
 }
--- packages/tap/test/tap.js
+++ packages/tap/test/tap.js
@@ -1,31 +1,125 @@
-contract("Tap", accounts => {
-  context("initialize", () => {
-    it("should initialize tap rate, collateral pool and vault", async () => {});
+const Agent = artifacts.require('Agent')
 
-    it("should revert on re-initialization", async () => {});
-  });
+const {
+  assertRevert,
+  assertInvalidOpcode
+} = require('@aragon/test-helpers/assertThrow')
+const { hash: namehash } = require('eth-ens-namehash')
+const ethUtil = require('ethereumjs-util')
+const getBalance = require('@aragon/test-helpers/balance')(web3)
+const web3Call = require('@aragon/test-helpers/call')(web3)
+const web3Sign = require('@aragon/test-helpers/sign')(web3)
 
-  context("withdraw", () => {
-    context("ETH", () => {
-      it("should transfer a tap-defined amount of ETH from the collateral pool to the vault", async () => {});
-    });
+const assertEvent = require('@aragon/test-helpers/assertEvent')
+const ethABI = new (require('web3-eth-abi')).AbiCoder()
+const getEvent = (receipt, event, arg) => {
+  return receipt.logs.filter(l => l.event == event)[0].args[arg]
+}
 
-    context("ERC20", () => {
-      it("should transfer a tap-defined amount of ERC20 from the collateral pool to the vault", async () => {});
-    });
+const ACL = artifacts.require('ACL')
+const AppProxyUpgradeable = artifacts.require('AppProxyUpgradeable')
+const EVMScriptRegistryFactory = artifacts.require('EVMScriptRegistryFactory')
+const DAOFactory = artifacts.require('DAOFactory')
+const Kernel = artifacts.require('Kernel')
+const KernelProxy = artifacts.require('KernelProxy')
 
-    it("it should revert if sender does not have 'WITHDRAW_ROLE'", async () => {});
-  });
+const EtherTokenConstantMock = artifacts.require('EtherTokenConstantMock')
+const DestinationMock = artifacts.require('DestinationMock')
+const KernelDepositableMock = artifacts.require('KernelDepositableMock')
 
-  context("updateTap", () => {
-    it("should update tap rate", async () => {});
-  });
+const NULL_ADDRESS = '0x00'
 
-  context("updateVault", () => {
-    it("should update vault address", async () => {});
-  });
+contract('Tap app', accounts => {
+  let daoFact, agentBase, dao, acl, agent, agentAppId
 
-  context("updateCollateralPool", () => {
-    it("should update collateral pool address", async () => {});
-  });
-});
+  let ETH,
+    ANY_ENTITY,
+    APP_MANAGER_ROLE,
+    EXECUTE_ROLE,
+    RUN_SCRIPT_ROLE,
+    ADD_PRESIGNED_HASH_ROLE,
+    DESIGNATE_SIGNER_ROLE,
+    ERC1271_INTERFACE_ID
+
+  const root = accounts[0]
+
+  before(async () => {
+    const kernelBase = await Kernel.new(true) // petrify immediately
+    const aclBase = await ACL.new()
+    const regFact = await EVMScriptRegistryFactory.new()
+    daoFact = await DAOFactory.new(
+      kernelBase.address,
+      aclBase.address,
+      regFact.address
+    )
+    agentBase = await Agent.new()
+
+    // Setup constants
+    ANY_ENTITY = await aclBase.ANY_ENTITY()
+    APP_MANAGER_ROLE = await kernelBase.APP_MANAGER_ROLE()
+    EXECUTE_ROLE = await agentBase.EXECUTE_ROLE()
+    RUN_SCRIPT_ROLE = await agentBase.RUN_SCRIPT_ROLE()
+    ADD_PRESIGNED_HASH_ROLE = await agentBase.ADD_PRESIGNED_HASH_ROLE()
+    DESIGNATE_SIGNER_ROLE = await agentBase.DESIGNATE_SIGNER_ROLE()
+    ERC1271_INTERFACE_ID = await agentBase.ERC1271_INTERFACE_ID()
+
+    const ethConstant = await EtherTokenConstantMock.new()
+    ETH = await ethConstant.getETHConstant()
+  })
+
+  beforeEach(async () => {
+    const r = await daoFact.newDAO(root)
+    dao = Kernel.at(getEvent(r, 'DeployDAO', 'dao'))
+    acl = ACL.at(await dao.acl())
+
+    await acl.createPermission(root, dao.address, APP_MANAGER_ROLE, root, {
+      from: root
+    })
+
+    // tap
+    tapAppId = namehash('fundraising-tap.aragonpm.test')
+
+    const agentReceipt = await dao.newAppInstance(
+      agentAppId,
+      agentBase.address,
+      '0x',
+      false
+    )
+    const agentProxyAddress = getEvent(agentReceipt, 'NewAppProxy', 'proxy')
+    agent = Agent.at(agentProxyAddress)
+
+    await agent.initialize()
+  })
+})
+
+// contract("Tap", accounts => {
+//   context("initialize", () => {
+//     it("should initialize tap rate, collateral pool and vault", async () => {});
+//
+//     it("should revert on re-initialization", async () => {});
+//   });
+//
+//   context("withdraw", () => {
+//     context("ETH", () => {
+//       it("should transfer a tap-defined amount of ETH from the collateral pool to the vault", async () => {});
+//     });
+//
+//     context("ERC20", () => {
+//       it("should transfer a tap-defined amount of ERC20 from the collateral pool to the vault", async () => {});
+//     });
+//
+//     it("it should revert if sender does not have 'WITHDRAW_ROLE'", async () => {});
+//   });
+//
+//   context("updateTap", () => {
+//     it("should update tap rate", async () => {});
+//   });
+//
+//   context("updateVault", () => {
+//     it("should update vault address", async () => {});
+//   });
+//
+//   context("updateCollateralPool", () => {
+//     it("should update collateral pool address", async () => {});
+//   });
+// });
