cb0d228753af62c1d96edb7152b287ad6e13e505
Olivier Sarrouy <olivier@pando.network>
2019-04-11 18:20:41 +0200

    Lint truffle.js

--- truffle.js
+++ truffle.js
@@ -1,38 +1,38 @@
 const mochaGasSettings = {
-  reporter: "eth-gas-reporter",
+  reporter: 'eth-gas-reporter',
   reporterOptions: {
-    currency: "USD",
-    gasPrice: 3
-  }
-};
+    currency: 'USD',
+    gasPrice: 3,
+  },
+}
 
-const mocha = process.env.GAS_REPORTER ? mochaGasSettings : {};
+const mocha = process.env.GAS_REPORTER ? mochaGasSettings : {}
 
 module.exports = {
   networks: {
     rpc: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
       network_id: 15,
       gas: 6.9e6,
-      gasPrice: 15000000001
+      gasPrice: 15000000001,
     },
     devchain: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*",
+      network_id: '*',
       gas: 6.9e6,
-      gasPrice: 15000000001
-    }
+      gasPrice: 15000000001,
+    },
   },
   mocha,
   compilers: {
     solc: {
-      version: "0.4.24",
+      version: '0.4.24',
       optimizer: {
         enabled: true,
-        runs: 10000
-      }
-    }
-  }
-};
+        runs: 10000,
+      },
+    },
+  },
+}
