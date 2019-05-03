19c19c1ce818bb2eeccd88fbc0a61dae6cbcc4c4
cd10012 <ced361@nyu.edu>
2019-05-03 16:38:20 +0200

    update view to new designs
    fix column header
    
    refactor

--- apps/apiary/app/src/screens/Orders.js
+++ apps/apiary/app/src/screens/Orders.js
@@ -1,68 +1,87 @@
-import { AddressField, Badge, Table, TableCell, TableHeader, TableRow, Text, Viewport } from '@aragon/ui'
+import { AddressField, Badge, Table, TableCell, TableHeader, TableRow, Text, ContextMenu, ContextMenuItem, SafeLink, theme } from '@aragon/ui'
 import React from 'react'
 import styled from 'styled-components'
-import Box from '../components/Box'
-import LocalIdentityBadge from '../components/LocalIdentityBadge'
 
 const orders = [
-  { id: 1, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'ANT', amount: 0.4, value: 0.8, price: '0.5' },
-  { id: 2, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'ANT', amount: 0.4, value: 0.8, price: '0.5' },
-  { id: 3, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'ETH', amount: 0.4, value: 0.8, price: '0.5' },
-  { id: 4, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'ETH', amount: 0.4, value: 0.8, price: '0.5' },
-  { id: 5, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'DAI', amount: 0.4, value: 0.8, price: '0.5' },
-  { id: 6, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'ANT', amount: 0.4, value: 0.8, price: '0.5' },
-  { id: 7, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'DAI', amount: 0.4, value: 0.8, price: '0.5' },
+  { id: 1, date: '25/03/2019', type: 'buy', from: '0x277bfcf7c2e162cb1ac3e9ae228a3132a75f83d4', collateral: 'ANT', amount: 0.4, price: '200.33', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
+  { id: 2, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'ANT', amount: 0.4, price: '4212.21', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
+  { id: 3, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'ETH', amount: 0.4, price: '2192.45', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
+  { id: 4, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'ETH', amount: 0.4, price: '20.50', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
+  { id: 5, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'DAI', amount: 0.4, price: '330.50', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
+  { id: 6, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'ANT', amount: 0.4, price: '977.25', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
+  { id: 7, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'DAI', amount: 0.4, price: '0.50', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'}
 ]
 
-export default class Repositories extends React.Component {
+export default class Orders extends React.Component {
+
+
   render() {
-    // const { repositories } = this.props
+    const getOrderStyles = (order) => {
+      let background, foreground, sign, type = ''
+      if (order.type === 'buy') {
+        background = theme.badgeAppBackground
+        foreground = theme.Purple
+        sign = '+'
+        type = 'Buying order'
+      } else {
+        background = theme.infoPermissionsBackground
+        foreground = 'rgb(218, 192, 139)'
+        sign = '-'
+        type = 'Selling order'
+      }
+
+      return { background, foreground, sign, type }
+    }
 
     return (
       <div>
         <Title>
-          <Text>Orders</Text>
+          <Text>Historical Orders</Text>
         </Title>
         <Table
           header={
             <TableRow>
               <TableHeader title="Date" />
-              <TableHeader title="Type" />
-              <TableHeader title="Source / recipient" />
+              <TableHeader title="Order Type" />
+              <TableHeader title="Address" />
               <TableHeader title="Amount" />
-              <TableHeader title="Collateral" />
-              <TableHeader title="Price" />
+              <TableHeader title="Rate" />
             </TableRow>
           }
         >
-          {orders.map(order => (
+          {orders.map((order) => {
+            const orderStyle = getOrderStyles(order)
+            return (
             <TableRow key={order.id}>
               <TableCell>
                 <Text>{order.date}</Text>
               </TableCell>
               <TableCell>
-                <Text color={order.type === 'buy' ? '#21d48e' : '#fb7777'}>{order.type}</Text>
+                <Badge background={orderStyle.background} foreground={orderStyle.foreground}>
+                  {orderStyle.type}
+                </Badge>
               </TableCell>
               <TableCell>
-                <Badge.Identity>{order.from}</Badge.Identity>
-              </TableCell>
-
-              <TableCell>
-                <Text color={order.type === 'buy' ? '#21d48e' : '#fb7777'}>
-                  {order.type === 'buy' ? '+' : '-'}
-                  {order.amount} BND
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>{order.value}</Text>
+                <AddressField address={order.from} />
               </TableCell>
               <TableCell>
                 <Text>
-                  {order.price} {order.collateral} / BND
+                  {orderStyle.sign}
+                  {order.amount + '  '}
+                  {order.collateral}
                 </Text>
               </TableCell>
+              <TableCell>
+                <Text>{'$' + order.price}</Text>
+                <ContextMenu>
+                  <SafeLink href={"https://etherscan.io/tx/" + order.txHash} target="_blank">
+                    <ContextMenuItem>View Tx on Etherscan</ContextMenuItem>
+                  </SafeLink>
+                 </ContextMenu>
+              </TableCell>
             </TableRow>
-          ))}
+            )})
+          }
         </Table>
       </div>
     )
@@ -70,7 +89,7 @@ export default class Repositories extends React.Component {
 }
 
 const Title = styled.h1`
-  margin: 20px 20px 10px 20px;
+  margin: 20px 0;
   font-weight: 600;
   display: flex;
   justify-content: space-between;
