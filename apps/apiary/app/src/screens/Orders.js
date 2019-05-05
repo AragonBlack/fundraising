7595cfc403876431d0cf657e2cb340ddc9ce1c67
Deam <deamkh@gmail.com>
2019-05-05 19:59:00 +0200

    Initial overview tab and chart implementation

--- apps/apiary/app/src/screens/Orders.js
+++ apps/apiary/app/src/screens/Orders.js
@@ -1,21 +1,87 @@
-import { AddressField, Badge, Table, TableCell, TableHeader, TableRow, Text, ContextMenu, ContextMenuItem, SafeLink, theme } from '@aragon/ui'
+import { AddressField, Badge, ContextMenu, ContextMenuItem, SafeLink, Table, TableCell, TableHeader, TableRow, Text, theme } from '@aragon/ui'
 import React from 'react'
 import styled from 'styled-components'
 
 const orders = [
-  { id: 1, date: '25/03/2019', type: 'buy', from: '0x277bfcf7c2e162cb1ac3e9ae228a3132a75f83d4', collateral: 'ANT', amount: 0.4, price: '200.33', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
-  { id: 2, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'ANT', amount: 0.4, price: '4212.21', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
-  { id: 3, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'ETH', amount: 0.4, price: '2192.45', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
-  { id: 4, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'ETH', amount: 0.4, price: '20.50', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
-  { id: 5, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'DAI', amount: 0.4, price: '330.50', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
-  { id: 6, date: '25/03/2019', type: 'sell', from: '0x88e4...', collateral: 'ANT', amount: 0.4, price: '977.25', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'},
-  { id: 7, date: '25/03/2019', type: 'buy', from: '0x88e4...', collateral: 'DAI', amount: 0.4, price: '0.50', txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005'}
+  {
+    id: 1,
+    date: '25/03/2019',
+    type: 'buy',
+    from: '0x277bfcf7c2e162cb1ac3e9ae228a3132a75f83d4',
+    collateral: 'ANT',
+    amount: 0.4,
+    price: '200.33',
+    txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005',
+  },
+  {
+    id: 2,
+    date: '25/03/2019',
+    type: 'buy',
+    from: '0x88e4...',
+    collateral: 'ANT',
+    amount: 0.4,
+    price: '4212.21',
+    txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005',
+  },
+  {
+    id: 3,
+    date: '25/03/2019',
+    type: 'sell',
+    from: '0x88e4...',
+    collateral: 'ETH',
+    amount: 0.4,
+    price: '2192.45',
+    txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005',
+  },
+  {
+    id: 4,
+    date: '25/03/2019',
+    type: 'buy',
+    from: '0x88e4...',
+    collateral: 'ETH',
+    amount: 0.4,
+    price: '20.50',
+    txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005',
+  },
+  {
+    id: 5,
+    date: '25/03/2019',
+    type: 'sell',
+    from: '0x88e4...',
+    collateral: 'DAI',
+    amount: 0.4,
+    price: '330.50',
+    txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005',
+  },
+  {
+    id: 6,
+    date: '25/03/2019',
+    type: 'sell',
+    from: '0x88e4...',
+    collateral: 'ANT',
+    amount: 0.4,
+    price: '977.25',
+    txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005',
+  },
+  {
+    id: 7,
+    date: '25/03/2019',
+    type: 'buy',
+    from: '0x88e4...',
+    collateral: 'DAI',
+    amount: 0.4,
+    price: '0.50',
+    txHash: '0xbd0a2fcb1143f1bb2c195965a776840240698bfe163f200173f9dc6b18211005',
+  },
 ]
 
 export default class Orders extends React.Component {
   render() {
-    const getOrderStyles = (order) => {
-      let background, foreground, sign, type = ''
+    const getOrderStyles = order => {
+      let background,
+        foreground,
+        sign,
+        type = ''
       if (order.type === 'buy') {
         background = theme.badgeAppBackground
         foreground = theme.Purple
@@ -47,39 +113,39 @@ export default class Orders extends React.Component {
             </TableRow>
           }
         >
-          {orders.map((order) => {
+          {orders.map(order => {
             const orderStyle = getOrderStyles(order)
             return (
-            <TableRow key={order.id}>
-              <TableCell>
-                <Text>{order.date}</Text>
-              </TableCell>
-              <TableCell>
-                <Badge background={orderStyle.background} foreground={orderStyle.foreground}>
-                  {orderStyle.type}
-                </Badge>
-              </TableCell>
-              <TableCell>
-                <AddressField address={order.from} />
-              </TableCell>
-              <TableCell>
-                <Text>
-                  {orderStyle.sign}
-                  {order.amount + '  '}
-                  {order.collateral}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>{'$' + order.price}</Text>
-                <ContextMenu>
-                  <SafeLink href={"https://etherscan.io/tx/" + order.txHash} target="_blank">
-                    <ContextMenuItem>View Tx on Etherscan</ContextMenuItem>
-                  </SafeLink>
-                 </ContextMenu>
-              </TableCell>
-            </TableRow>
-            )})
-          }
+              <TableRow key={order.id}>
+                <TableCell>
+                  <Text>{order.date}</Text>
+                </TableCell>
+                <TableCell>
+                  <Badge background={orderStyle.background} foreground={orderStyle.foreground}>
+                    {orderStyle.type}
+                  </Badge>
+                </TableCell>
+                <TableCell>
+                  <AddressField address={order.from} />
+                </TableCell>
+                <TableCell>
+                  <Text>
+                    {orderStyle.sign}
+                    {order.amount + '  '}
+                    {order.collateral}
+                  </Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{'$' + order.price}</Text>
+                  <ContextMenu>
+                    <SafeLink href={'https://etherscan.io/tx/' + order.txHash} target="_blank">
+                      <ContextMenuItem>View Tx on Etherscan</ContextMenuItem>
+                    </SafeLink>
+                  </ContextMenu>
+                </TableCell>
+              </TableRow>
+            )
+          })}
         </Table>
       </div>
     )
@@ -87,8 +153,6 @@ export default class Orders extends React.Component {
 }
 
 const Title = styled.h1`
-  margin: 20px 0;
+  margin-bottom: 1rem;
   font-weight: 600;
-  display: flex;
-  justify-content: space-between;
 `
--- apps/apiary/app/src/screens/Overview.js
+++ apps/apiary/app/src/screens/Overview.js
@@ -1,11 +1,106 @@
-import { AddressField, Table, TableCell, TableHeader, TableRow, Text, Viewport } from '@aragon/ui'
+import { Table, TableCell, TableRow, Text } from '@aragon/ui'
 import React from 'react'
-import Box from '../components/Box'
+import styled from 'styled-components'
+import antImage from '../assets/ant.png'
+import ethImage from '../assets/eth.png'
+import Chart from '../components/Chart'
 
 export default class Repositories extends React.Component {
   render() {
-    // const { repositories } = this.props
-
-    return <div>Overview</div>
+    return (
+      <div>
+        <TokenBalances>
+          <h1 className="title">
+            <Text>Token balances</Text>
+          </h1>
+          <Table>
+            <TableRow>
+              <TableCell>
+                <div className="cell-content">
+                  <p className="title">Bonded token supply</p>
+                  <p className="number">5600</p>
+                  <p className="sub-number">$123,600,923.82</p>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="cell-content">
+                  <div className="title">
+                    <img src={ethImage} />
+                    <p>ETH Collateral</p>
+                  </div>
+                  <p className="number">103,039.39</p>
+                  <p className="sub-number">$76,600,923.82</p>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="cell-content">
+                  <div className="title">
+                    <img src={antImage} />
+                    <p>ANT Collateral</p>
+                  </div>
+                  <p className="number">2,934.45</p>
+                  <p className="sub-number">$17,586.27</p>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="cell-content">
+                  <p className="title">Total balance USD</p>
+                  <p className="number">$23,699,746.32</p>
+                  <p className="sub-number">$123,600,923.82</p>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="cell-content">
+                  <p className="title">Tap rate</p>
+                  <p className="number">11,340</p>
+                  <p className="sub-number">$25,500.82</p>
+                </div>
+              </TableCell>
+            </TableRow>
+          </Table>
+        </TokenBalances>
+        <Chart />
+      </div>
+    )
   }
 }
+
+const TokenBalances = styled.div`
+  margin-bottom: 2rem;
+
+  table {
+    box-shadow: 0px 6px 14px rgba(0, 0, 0, 0.06);
+  }
+
+  .title {
+    margin-bottom: 1rem;
+    font-weight: 600;
+  }
+
+  .cell-content {
+    img {
+      display: inline-block;
+      height: 16px;
+      margin-right: 0.5rem;
+    }
+
+    .title {
+      display: flex;
+      font-weight: 600;
+      color: #6d777b;
+      text-transform: uppercase;
+      opacity: 0.7;
+      white-space: nowrap;
+    }
+
+    .number {
+      margin: 0.75rem 0;
+      font-size: 32px;
+      line-height: 24px;
+    }
+
+    .sub-number {
+      color: #b5b5b5;
+    }
+  }
+`
