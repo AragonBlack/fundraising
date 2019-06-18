98d958d8fa6f4b778cb5a0ca47f036f39b2c3716
cd10012 <ced361@nyu.edu>
2019-06-18 13:25:33 +0200

    asset conversion input

--- apps/controller-aragon-fundraising/app/src/components/NewOrderSidePanel.js
+++ apps/controller-aragon-fundraising/app/src/components/NewOrderSidePanel.js
@@ -1,8 +1,32 @@
-import { Button, DropDown, Field, SidePanel, TabBar, Text, TextInput } from '@aragon/ui'
+import { Button, DropDown, Field, SidePanel, TabBar, Table, TableCell, TableRow, Text, TextInput } from '@aragon/ui'
 import React from 'react'
 import styled from 'styled-components'
+import transferArrows from '../assets/transferArrows.png'
 
 const collateralTokens = ['DAI', 'ANT']
+
+const styles = {
+  selectionInputLeft: {
+    borderRadius: '3px 0px 0px 3px',
+    textAlign: 'right',
+    padding: 0,
+    paddingLeft: 0
+  },
+  selectionInputRight: {
+    textAlign: 'right',
+    paddingRight: '50px'
+  },
+  noBorderCell: {
+    borderBottom: 0,
+    borderTop: 0,
+    padding: 0
+  },
+  maxBalanceLink: {
+    paddingLeft: '14px',
+    textDecoration: 'underline',
+    cursor: 'pointer'
+  }
+}
 
 export default class NewOrderSidePanel extends React.Component {
   static defaultProps = {
@@ -58,27 +82,72 @@ export default class NewOrderSidePanel extends React.Component {
         <TabBarWrapper>
           <TabBar items={['Buy', 'Sell']} selected={activeTab} onChange={idx => this.setState({ activeTab: idx })} />
         </TabBarWrapper>
-        <Text size={'small'}>Price: {'$' + price}</Text>
         <Form onSubmit={this.handleSubmit}>
-          <Field label="Amount">
-            <TextInput type="number" ref={amount => (this.amountInput = amount)} value={amount} onChange={this.handleAmountChange} required wide />
-          </Field>
-          <Field label="Collateral Token">
-            <DropDown items={collateralTokens} active={activeItem} onChange={this.handleTokenChange} />
-          </Field>
+          <Table noSideBorders={true} >
+            <TableRow>
+              <TableCell style={styles.noBorderCell}>
+                <Field css={fieldCss} label="Order Amount">
+                  <TextInput
+                    adornment={<a style={styles.maxBalanceLink}>MAX</a>}
+                    type="number"
+                    style={styles.selectionInputLeft}
+                    ref={amount => (this.amountInput = amount)}
+                    value={amount}
+                    onChange={this.handleAmountChange}
+                    wide
+                    required
+                  />
+                </Field>
+                <StyledDropdown>
+                  <DropDown items={collateralTokens} active={activeItem}  onChange={this.handleTokenChange} />
+                </StyledDropdown>
+              </TableCell>
+              <TableCell style={styles.noBorderCell}>
+                <img src={transferArrows} style={{ width: '40px', display: 'flex' }} />
+                <Field css={fieldCss} label="Token Amount">
+                  <TextInput
+                    style={styles.selectionInputRight}
+                    adornment={<span style={{ paddingRight: '14px'}}>ANT</span>}
+                    adornmentPosition={"end"}
+                    ref={amount => (this.amountInput = amount)}
+                    value={amount}
+                    onChange={this.handleAmountChange}
+                    required
+                    wide
+                  />
+                </Field>
+              </TableCell>
+            </TableRow>
+          </Table>
           <Button mode="strong" type="submit" wide onClick={onSubmit}>
-            {activeTab === 0 ? 'Create buy order' : 'Create sell order'}
+            {activeTab === 0 ? 'Place buy order' : 'Place sell order'}
           </Button>
         </Form>
       </SidePanel>
     )
   }
 }
-
+const StyledDropdown = styled.div`
+  > div {
+    box-shadow: inset 0 1px 2px rgba(0,0,0,0.06);
+  }
+  > div > div {
+    padding-right: 26px;
+    border-radius: 0 3px 3px 0;
+    border-left: 0px;
+    margin-top: 1px;
+  }
+`
 const Form = styled.form`
   margin-top: 20px;
 `
 
 const TabBarWrapper = styled.div`
   margin: 0 -30px 30px;
+`
+
+const fieldCss = `
+  > label > div {
+    width: auto;
+  }
 `
