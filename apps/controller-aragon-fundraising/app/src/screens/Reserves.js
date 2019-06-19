298e916881ea178d47f8a190d8f83445e7d94129
cd10012 <ced361@nyu.edu>
2019-06-19 15:10:50 +0200

    make page responsive / add event handlers

--- apps/controller-aragon-fundraising/app/src/screens/Reserves.js
+++ apps/controller-aragon-fundraising/app/src/screens/Reserves.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Badge, Button, Card, Form, Field, Table, TableRow, TableCell, Text, TextInput } from '@aragon/ui'
+import React, { useEffect, useRef, useState } from 'react'
+import { Badge, Button, Card, Form, Field, Table, TableRow, TableCell, Text, TextInput, theme } from '@aragon/ui'
 import styled from 'styled-components'
 
 export default () => {
@@ -12,45 +12,66 @@ export default () => {
   })
 
   const { transferable, monthlyAllocation, totalSupply, antRatio, daiRatio } = state
+  const inputRef = useRef(monthlyAllocation)
+
+  const handleMonthlyChange = event => {
+    setState({...state, monthlyAllocation: event.target.value })
+  }
+  const onButtonClick = () => {
+    const updatedAllocation = inputRef.current.value;
+    // call contract api to update if they have valid permissions
+  };
+
   return (
     <ContentWrapper>
-      <Card width={"auto"}>
+      <Card style={{ minWidth: '500px' }} width={"50%"}>
         <h1 className="title">
           <Text>Edit reserve settings</Text>
         </h1>
-        <Table width={"400"} >
+        <Table style={{ width: '50%', float: 'left', marginRight: '2rem' }}>
           <TableRow>
-            <Field title="Monthly allocation">
+            <Field
+              css={inputCss}
+              label={NotificationLabel("Monthly allocation")}>
               <TextInput
-                adornment={"DAI"}
+                ref={inputRef}
+                adornment={<Text as="p" style={{ paddingRight: '12px' }}>DAI</Text>}
                 adornmentPosition={"end"}
                 value={monthlyAllocation}
+                onChange={handleMonthlyChange}
                 required
-                wide
               />
             </Field>
           </TableRow>
           <TableRow>
-            <Button type="submit">Edit monthly allocation</Button>
+            <Button type="submit" onSubmit={onButtonClick}>Edit monthly allocation</Button>
           </TableRow>
         </Table>
         <div>
-          <Field title="ANT collateralization ratio">
-            {antRatio}%
+          <Field label={NotificationLabel("ANT collateralization ratio")}>
+            <Text>{antRatio}%</Text>
           </Field>
-          <Field title="DAI collateralization ratio">
-            {daiRatio}%
+          <Field label={NotificationLabel("DAI collateralization ratio")}>
+            <Text>{daiRatio}%</Text>
           </Field>
         </div>
       </Card>
-      <Card width={"auto"}>
+      <Card width={"20%"}>
         <CardHeader>
-          <Text smallcaps >Monthly Allowance</Text>
+          <Text
+            color={theme.textSecondary}
+            size="xlarge"
+            smallcaps
+            as="p"
+            style={{ padding: '5px 20px' }}
+          >
+            Monthly Allowance
+          </Text>
         </CardHeader>
-        <Table noSideBorders >
+        <Table css={borderlessTableCss} noSideBorders >
           <TableRow>
             <TableCell>
-              <Text>Total Supply</Text>
+              <Text color={theme.textSecondary}>Total Supply</Text>
             </TableCell>
             <TableCell>
               <Text>{totalSupply}</Text>
@@ -58,7 +79,7 @@ export default () => {
           </TableRow>
           <TableRow>
             <TableCell>
-              <Text>Transferable</Text>
+              <Text color={theme.textSecondary}>Transferable</Text>
             </TableCell>
             <TableCell>
               <Text>{transferable ? 'YES' : 'NO'}</Text>
@@ -66,7 +87,7 @@ export default () => {
           </TableRow>
           <TableRow>
             <TableCell>
-              <Text>Token</Text>
+              <Text color={theme.textSecondary}>Token</Text>
             </TableCell>
             <TableCell>
               <Badge>Ethical (ETH)</Badge>
@@ -78,9 +99,33 @@ export default () => {
   )
 }
 
+const styles = {
+  notification: {
+    margin: '0 10px',
+    cursor: 'pointer'
+  }
+}
+
+const NotificationLabel = (label) => (
+  <Text style={{ marginBottom: '10px'}}>
+    {label}
+    <Badge.Notification
+      style={{ margin: '0 10px', cursor: 'pointer'}}
+      background={theme.badgeAppForeground}
+    >
+      ?
+    </Badge.Notification>
+  </Text>
+)
+
 const ContentWrapper = styled.div`
   padding: 2rem;
   display:flex;
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+
 
   > div {
     float:left;
@@ -91,13 +136,34 @@ const ContentWrapper = styled.div`
     padding: 2rem;
   }
 
+  > div:last-child {
+    margin-left: 1.5rem;
+    min-width: 260px;
+  }
+
   @media only screen and (max-width: 700px) {
     padding: 0;
+  }
+`
+
+const inputCss = `
+  > label > div:last-child {
+    width: 100%;
+  }
+
+  > label > div input {
+    width: 100%;
+  }
+`
+
+const borderlessTableCss = `
+  tbody tr td {
+    border: none;
+    padding: 10px 20px;
   }
 `
 
 const CardHeader = styled.div`
   width: 100%;
   height: auto;
-  border-bottom: 1px solid rgba(209, 209, 209, 0.5);
 `
