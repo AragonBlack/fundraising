20cb7eac31b6e055a0436f22f5c586576b63f9fb
cd10012 <ced361@nyu.edu>
2019-07-01 05:14:28 +0200

    reduce order state to use big numbers for values
    begin refactoring dao deployment script

--- apps/controller-aragon-fundraising/app/src/reducer.js
+++ apps/controller-aragon-fundraising/app/src/reducer.js
@@ -1,7 +1,26 @@
-function appStateReducer(state) {
-  // const { tokenSupply, tapRate, price, historicalOrders, cache } = state || {}
+import BigNumber from 'bignumber.js'
 
-  return state
+function appStateReducer(state) {
+  const { historicalOrders } = state || {}
+
+  const ordersBN = historicalOrders
+    ? historicalOrders.map(order => ({
+        ...order,
+        amount: new BigNumber(order.amount),
+        // Note that numbers in `numData` are not safe for accurate
+        // computations (but are useful for making divisions easier) aka price data.
+        numData: {
+          amount: parseInt(order.amount, 10),
+        },
+      }))
+    : []
+
+  // TODO: update the token balances for the bondedToken for each user
+
+  return {
+    ...state,
+    historicalOrders: ordersBN,
+  }
 }
 
 export default appStateReducer
--- apps/kit-aragon-fundraising/scripts/deploy_dao.js
+++ apps/kit-aragon-fundraising/scripts/deploy_dao.js
@@ -1,28 +1,88 @@
+require('dotenv').config({ path: '../.env' })
+const path = require('path')
 const FundraisingKit = artifacts.require('FundraisingKit')
 const TokenMock = artifacts.require('TokenMock')
+const pct16 = x =>
+  new web3.BigNumber(x).times(new web3.BigNumber(10).toPower(16))
+const getEventResult = (receipt, event, param) =>
+  receipt.logs.filter(l => l.event == event)[0].args[param]
 
-const deploy = async address => {
-  const kit = await FundraisingKit.at(address)
+// Ensure that these address are up to date according to the network
+// Defaults set here are for the local rpc
+const defaultOwner = process.env.OWNER || '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7'
+const defaultFundraisingKitAddr = process.env.FUNDRAISING_KIT || process.argv[6]
+const defaultENSAddress = process.env.ENS || '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1'
+const defaultHolders = [defaultOwner, '0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb']
 
-  console.log('tata')
+const getDeployedKit = async address => {
+  return await FundraisingKit.at(address)
 }
 
-module.exports = async callback => {
-  const collateral1 = await TokenMock.new('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', 1000000000000000000)
-  const collateral2 = await TokenMock.new('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', 1000000000000000000)
+// TODO: paramertize the script to accept information needed for the tokens, voting app, etc.
+module.exports = async (
+  callback,
+  { owner = defaultOwner, fundraisingKitAddr = defaultFundraisingKitAddr, initialHolders = defaultHolders, tokenSupply = 1000000000000000000 }
+) => {
+  console.log(`Creating token instances with owner: ${owner} and supply: ${tokenSupply}`)
+  const multisigToken = await TokenMock.new(owner, tokenSupply)
+  const bondedToken = await TokenMock.new(owner, tokenSupply)
+  console.log(`Collateral tokens deployed @ ${collateral1.address} and ${collateral2.address}`)
 
-  const kit = await FundraisingKit.at(process.argv[6])
+  if (fundraisingKitAddr) {
+    console.log('Creating kit instance at ', fundraisingKitAddr)
+  } else {
+    // Retrieve the lastest version from APM
+  }
 
-  const receipt1 = await kit.newTokens('PRO', 'PROJECT')
-  const receipt2 = await kit.newMultisigInstance(
+  const kit = getDeployedKit(fundraisingKitAddr)
+  console.log('Kit instance created')
+
+  const receiptTokens = await kit.newTokens('PRO', 'PROJECT')
+  const receiptMultisig = await kit.newMultisigInstance(
     'fundraising' + Math.random(),
-    ['0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', '0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb '],
+    initialHolders,
     2
   )
-  const receipt3 = await kit.newFundraisingInstance(collateral1.address, collateral2.address)
-  const dao = receipt2.logs.filter(l => l.event == 'DeployMultisigInstance')[0].args.dao
+  const receiptDao = await kit.newFundraisingInstance(multisigToken.address, bondedToken.address)
 
-  console.log('DAO deployed at ' + dao)
+  const daoAddress = getEventResult(receiptDao, 'DeployFundraising', 'dao')
+  const cacheAddress = getEventResult(receiptMultisig, 'DeployMultisigInstance', 'dao') // multisig.dao === dao
+  const bondedTokenAddress = getEventResult(bondedToken, 'DeployToken', 'token')
+  const multisigTokenAddress = getEventResult(multisigToken, 'DeployToken', 'token')
+
+  // Installed apps
+  const controllerAddress = getEventResult(receiptDao, 'DeployInstance', 'fundraising-controller-aragon-fundraising')
+  const bancorAddress = getEventResult(receiptDao, 'DeployInstance', 'fundraising-formula-bancor')
+  const marketMakerAddress = getEventResult(receiptDao, 'DeployInstance', 'fundraising-market-maker-bancor')
+  const bondedVotingAddress = getEventResult(receiptDao, 'DeployInstance', 'voting')
+  const bondedTokenManagerAddress = getEventResult(receiptDao, 'DeployInstance', 'token-manager')
+  const tapAddress = getEventResult(receiptDao, 'DeployInstance', 'fundraising-module-tap')
+  const poolAddress = getEventResult(receiptDao, 'DeployInstance', 'pool')
+  const votingAddress = getEventResult(receiptDAO, 'DeployInstance', 'voting')
+
+  // Multisig DAO configuration
+  const tokenManagerAddress = getEventResult(receiptMultisig, 'DeployInstance', 'token-manager')
+  const financeAddress = getEventResult(receiptMultisig, 'DeployInstance', 'finance')
+  const vaultAddress  = getEventResult(receiptMultisig, 'DeployInstance', 'vault')
+  const votingMultisigAddress = getEventResult(receiptMultisig, 'DeployInstance', 'voting')
+
+  if (daoAddress === cacheAddress) {
+    console.log('DAO deployed at ' + dao)
+  } else {
+    throw new Error('Setting up the DAO cache failed')
+  }
+
+  console.log('DAO Created:', daoAddress)
+  console.log('TokenManager (curve-bonded) Address:', bondedTokenManagerAddress)
+  console.log('Voting (curve-bonded) Address:', bondedVotingAddress)
+  console.log('Market Maker Address:', marketMakerAddress)
+  console.log('BancorFormula Address:', bancorAddress)
+  console.log('Pool Address:', poolAddres)
+  console.log('Tap Address:', tapAddress)
+  console.log('Finance Address:', financeAddress)
+  console.log('Vault Address:', vaultAddress)
+  console.log('Voting (curve-bonded) Address:', votingAddress)
+  console.log('Voting (multisig) Address:', votingMultisigAddress)
 
   callback()
 }
