3cc3af827a9a40677a2d07bd19037101010053b4
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-07-10 12:19:55 +0200

    Update comments and add some slight gas optimization

--- apps/module-pool/contracts/Pool.sol
+++ apps/module-pool/contracts/Pool.sol
@@ -7,8 +7,11 @@ pragma solidity 0.4.24;
 import "@aragon/apps-agent/contracts/Agent.sol";
 
 
-// `Agent` is already an `AragonApp` [so `Pool` still is too]
-// The `initialize` function is implemented in the Agent contract
+/*
+ * NOTE: `Agent` is already an `AragonApp` [so `Pool` is too]
+ * NOTE: the `initialize` function is implemented in the Agent contract
+*/
+
 contract Pool is Agent {
     /* Hardcoded constants to save gas
     bytes32 public constant SAFE_EXECUTE_ROLE = keccak256("SAFE_EXECUTE_ROLE");
@@ -18,7 +21,6 @@ contract Pool is Agent {
     bytes32 public constant SAFE_EXECUTE_ROLE = 0x0a1ad7b87f5846153c6d5a1f761d71c7d0cfd122384f56066cd33239b7933694;
     bytes32 public constant ADD_PROTECTED_TOKEN_ROLE = 0x6eb2a499556bfa2872f5aa15812b956cc4a71b4d64eb3553f7073c7e41415aaa;
     bytes32 public constant REMOVE_PROTECTED_TOKEN_ROLE = 0x71eee93d500f6f065e38b27d242a756466a00a52a1dbcd6b4260f01a8640402a;
-    
 
     string private constant ERROR_TOKENS_CAP_REACHED = "POOL_TOKENS_CAP_REACHED";
     string private constant ERROR_TOKEN_NOT_ETH_OR_CONTRACT = "POOL_TOKEN_NOT_ETH_OR_CONTRACT";
@@ -38,10 +40,10 @@ contract Pool is Agent {
     /***** external functions *****/
 
     /**
-    * @notice Safe execute '`@radspec(_target, _data)`' on `_target`
-    * @param _target Address where the action is being executed
-    * @param _data Calldata for the action
-    * @return Exits call frame forwarding the return data of the executed call (either error or success data)
+     * @notice Safe execute '`@radspec(_target, _data)`' on `_target`
+     * @param _target Address where the action is to be executed
+     * @param _data Calldata for the action to be executed
+     * @return Exits call frame forwarding the return data of the executed call [either error or success data]
     */
     function safeExecute(address _target, bytes _data) external auth(SAFE_EXECUTE_ROLE) {
         address[] memory _protectedTokens = new address[](protectedTokens.length);
@@ -53,7 +55,9 @@ contract Pool is Agent {
             address token = protectedTokens[i];
             // we don't care if target is ETH [0x00...0] as it can't be spent anyhow [though you can't invoke anything at 0x00...0]
             require(_target != token || token == ETH, ERROR_TARGET_PROTECTED);
+            // we copy the protected tokens array to check whether the storage array has been modified during the underlying call
             _protectedTokens[i] = token;
+            // we copy the balances to check whether they have been modified during the underlying call
             balances[i] = balance(token);
         }
 
@@ -62,13 +66,13 @@ contract Pool is Agent {
         assembly {
             size := returndatasize
             ptr := mload(0x40)
-            // new "memory end"
             mstore(0x40, add(ptr, size))
             returndatacopy(ptr, 0, size)
         }
 
         if (result) {
-            // if the underlying call has succeeded, check protected tokens' balances and return the call's return data
+            // if the underlying call has succeeded, we check that the protected tokens
+            // and their balances have not been modified and return the call's return data
             for (uint256 j = 0; j < _protectedTokens.length; j++) {
                 require(protectedTokens[j] == _protectedTokens[j], ERROR_PROTECTED_TOKENS_MODIFIED);
                 require(balances[j] == balance(_protectedTokens[j]), ERROR_BALANCE_NOT_CONSTANT);
@@ -80,7 +84,7 @@ contract Pool is Agent {
                 return(ptr, size)
             }
         } else {
-            // if the underlying call has failed, revert and forward [possible] returned error data
+            // if the underlying call has failed, we revert and forward [possible] returned error data
             assembly {
                 revert(ptr, size)
             }
@@ -88,20 +92,20 @@ contract Pool is Agent {
     }
 
     /**
-    * @notice Add `_token.symbol(): string` to the list of protected tokens
-    * @param _token Address of the token to be protected
+     * @notice Add `_token.symbol(): string` to the list of protected tokens
+     * @param _token Address of the token to be protected
     */
     function addProtectedToken(address _token) external auth(ADD_PROTECTED_TOKEN_ROLE) {
         require(protectedTokens.length < PROTECTED_TOKENS_CAP, ERROR_TOKENS_CAP_REACHED);
-        require(_token == ETH || isContract(_token), ERROR_TOKEN_NOT_ETH_OR_CONTRACT);
+        require(isContract(_token) || _token == ETH, ERROR_TOKEN_NOT_ETH_OR_CONTRACT);
         require(!isTokenProtected(_token), ERROR_TOKEN_ALREADY_PROTECTED);
 
         _addProtectedToken(_token);
     }
 
     /**
-    * @notice Remove `_token.symbol(): string` from the list of protected tokens
-    * @param _token Address of the token to be unprotected
+     * @notice Remove `_token.symbol(): string` from the list of protected tokens
+     * @param _token Address of the token to be unprotected
     */
     function removeProtectedToken(address _token) external auth(REMOVE_PROTECTED_TOKEN_ROLE) {
         require(isTokenProtected(_token), ERROR_TOKEN_NOT_PROTECTED);
