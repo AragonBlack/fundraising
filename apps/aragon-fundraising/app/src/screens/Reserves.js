028a492b5b735b5550117defbc6c9c67752886a4
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-08-21 20:13:34 +0200

    Update tooltip

--- apps/aragon-fundraising/app/src/screens/Reserves.js
+++ apps/aragon-fundraising/app/src/screens/Reserves.js
@@ -11,9 +11,9 @@ import { round, fromDecimals, toMonthlyAllocation } from '../lib/math-utils'
 
 // In this copy we should display the user the percentage of max increase of the tap
 const hoverTextNotifications = [
-  'This will update the monthly allocation (tap rate) i.e. how much funds can be released within the bonding curve contract per 30-day period. Note: this value must be less than the max increase limit set inside the contract.',
-  "You're essentially bonding collateral when buying tokens (increasing the supply), and burning collateral when selling tokens (decreasing the supply). These relationships are defined by the smart contract.",
-  'FILL ME PLZ!!', // TODO: add floor notification
+  'The tap defines the amount of funds which can be released every month out of the market-maker reserve to the beneficiary of the fundraising campaign.',
+  'The reserve ratio defines the ratio between the amount of collateral in your market-maker reserve and the market cap of the fundraising campaign.',
+  'The floor defines the amount of funds which must be kept in the market-maker reserve regardless of the tap rate.', // TODO: add floor notification
 ]
 
 const buttonStyle = `
