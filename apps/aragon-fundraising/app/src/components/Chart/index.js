a6d6050c27c2e105c6aab2d2dbe017f73865e297
deam <deamme@users.noreply.github.com>
2019-08-21 18:19:34 +0200

    Add empty state screens

--- apps/aragon-fundraising/app/src/components/Chart/index.js
+++ apps/aragon-fundraising/app/src/components/Chart/index.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import { startOfDay, endOfDay } from 'date-fns'
 import DateRangeInput from '../DateRange/DateRangeInput'
 import { filter } from './utils'
+import EmptyOrders from '../../assets/EmptyOrders.svg'
 
 const bondingCurveData = [...Array(600).keys()].map(idx => ({
   tokens: idx + 1,
@@ -22,79 +23,101 @@ export default ({ batches }) => {
     end: endOfDay(new Date()),
   })
 
-  return (
-    <Chart>
-      <div className="navbar">
-        {activeItem === 1 ? (
-          <div className="timeline">
-            <div>
-              <div className={activeNavItem === 0 ? 'item active' : 'item'} onClick={() => setActiveNavItem(0)}>
-                <span>1</span>
-                <span>H</span>
-              </div>
-              <div className={activeNavItem === 1 ? 'item active' : 'item'} onClick={() => setActiveNavItem(1)}>
-                <span>1</span>
-                <span>D</span>
-              </div>
-              <div className={activeNavItem === 2 ? 'item active' : 'item'} onClick={() => setActiveNavItem(2)}>
-                <span>1</span>
-                <span>M</span>
-              </div>
-              <div className={activeNavItem === 3 ? 'item active' : 'item'} onClick={() => setActiveNavItem(3)}>
-                <span>1</span>
-                <span>Y</span>
-              </div>
-              <span className={activeNavItem === 4 ? 'item active' : 'item'} onClick={() => setActiveNavItem(4)}>
-                ALL
-              </span>
-            </div>
-            <DateRangeInput
-              startDate={date.start}
-              endDate={date.end}
-              onClick={() => setActiveNavItem(5)}
-              onChange={date => setDate(date)}
-              active={activeNavItem === 5}
-            />
-          </div>
-        ) : (
-          <div />
-        )}
-        {/* <div className="chart-view">
-          <p className="chart-view-text">Chart view</p>
-          <DropDown items={items} selected={activeItem} onChange={index => setActiveItem(index)} />
-        </div> */}
-      </div>
-      {activeItem === 0 && (
-        <ResponsiveContainer height={400}>
-          <AreaChart margin={{ left: 40, bottom: 40, top: 40, right: 40 }} height={400} data={bondingCurveData}>
-            <defs>
-              <linearGradient id="colorBlue" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stopColor="#08BEE5" stopOpacity={0.3} />
-                <stop offset="100%" stopColor="#08BEE5" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <CartesianGrid strokeDasharray="8 8" vertical={false} />
+  console.log(batches)
 
-            <XAxis type="number" dataKey="tokens" hide={false} interval="preserveStartEnd" tickMargin={25} tickLine={false} axisLine={false} tickCount={5} />
-            <YAxis tickMargin={25} tickLine={false} axisLine={false} />
-            <ReferenceDot isFront x={210} y={882} r={6} fill="#08BEE5" stroke="none" />
-            <Tooltip labelFormatter={value => 'Bonded tokens: ' + value} />
-            <Area isAnimationActive strokeWidth={2} type="monotone" dataKey="price" stroke="#08BEE5" fillOpacity={1} fill="url(#colorBlue)" />
-          </AreaChart>
-        </ResponsiveContainer>
+  return (
+    <>
+      {!batches.length && (
+        <EmptyState>
+          <img src={EmptyOrders} />
+          <p css="font-size: 24px; margin-top: 1rem;">No data to show.</p>
+        </EmptyState>
       )}
-      {activeItem === 1 && (
-        <ResponsiveContainer width="100%" height={400}>
-          <BarChart margin={{ left: 40, bottom: 40, top: 40, right: 40 }} data={filter(batches, activeNavItem, date)} barSize={20}>
-            <Tooltip cursor={{ fill: '#08BEE5', fillOpacity: '0.2' }} />
-            <CartesianGrid strokeDasharray="8 8" vertical={false} />
-            <XAxis dataKey="date" minTickGap={100} interval="preserveStartEnd" tickMargin={25} tickLine={false} axisLine={false} />
-            <YAxis tickMargin={25} tickLine={false} axisLine={false} />
-            <Bar isAnimationActive dataKey="price" fill="#08BEE5" />
-          </BarChart>
-        </ResponsiveContainer>
+
+      {!!batches.length && (
+        <Chart>
+          <div className="navbar">
+            {activeItem === 1 ? (
+              <div className="timeline">
+                <div>
+                  <div className={activeNavItem === 0 ? 'item active' : 'item'} onClick={() => setActiveNavItem(0)}>
+                    <span>1</span>
+                    <span>H</span>
+                  </div>
+                  <div className={activeNavItem === 1 ? 'item active' : 'item'} onClick={() => setActiveNavItem(1)}>
+                    <span>1</span>
+                    <span>D</span>
+                  </div>
+                  <div className={activeNavItem === 2 ? 'item active' : 'item'} onClick={() => setActiveNavItem(2)}>
+                    <span>1</span>
+                    <span>M</span>
+                  </div>
+                  <div className={activeNavItem === 3 ? 'item active' : 'item'} onClick={() => setActiveNavItem(3)}>
+                    <span>1</span>
+                    <span>Y</span>
+                  </div>
+                  <span className={activeNavItem === 4 ? 'item active' : 'item'} onClick={() => setActiveNavItem(4)}>
+                    ALL
+                  </span>
+                </div>
+                <DateRangeInput
+                  startDate={date.start}
+                  endDate={date.end}
+                  onClick={() => setActiveNavItem(5)}
+                  onChange={date => setDate(date)}
+                  active={activeNavItem === 5}
+                />
+              </div>
+            ) : (
+              <div />
+            )}
+            {/* <div className="chart-view">
+<p className="chart-view-text">Chart view</p>
+<DropDown items={items} selected={activeItem} onChange={index => setActiveItem(index)} />
+</div> */}
+          </div>
+          {activeItem === 0 && (
+            <ResponsiveContainer height={400}>
+              <AreaChart margin={{ left: 40, bottom: 40, top: 40, right: 40 }} height={400} data={bondingCurveData}>
+                <defs>
+                  <linearGradient id="colorBlue" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="0%" stopColor="#08BEE5" stopOpacity={0.3} />
+                    <stop offset="100%" stopColor="#08BEE5" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+                <CartesianGrid strokeDasharray="8 8" vertical={false} />
+
+                <XAxis
+                  type="number"
+                  dataKey="tokens"
+                  hide={false}
+                  interval="preserveStartEnd"
+                  tickMargin={25}
+                  tickLine={false}
+                  axisLine={false}
+                  tickCount={5}
+                />
+                <YAxis tickMargin={25} tickLine={false} axisLine={false} />
+                <ReferenceDot isFront x={210} y={882} r={6} fill="#08BEE5" stroke="none" />
+                <Tooltip labelFormatter={value => 'Bonded tokens: ' + value} />
+                <Area isAnimationActive strokeWidth={2} type="monotone" dataKey="price" stroke="#08BEE5" fillOpacity={1} fill="url(#colorBlue)" />
+              </AreaChart>
+            </ResponsiveContainer>
+          )}
+          {activeItem === 1 && (
+            <ResponsiveContainer width="100%" height={400}>
+              <BarChart margin={{ left: 40, bottom: 40, top: 40, right: 40 }} data={filter(batches, activeNavItem, date)} barSize={20}>
+                <Tooltip cursor={{ fill: '#08BEE5', fillOpacity: '0.2' }} />
+                <CartesianGrid strokeDasharray="8 8" vertical={false} />
+                <XAxis dataKey="date" minTickGap={100} interval="preserveStartEnd" tickMargin={25} tickLine={false} axisLine={false} />
+                <YAxis tickMargin={25} tickLine={false} axisLine={false} />
+                <Bar isAnimationActive dataKey="price" fill="#08BEE5" />
+              </BarChart>
+            </ResponsiveContainer>
+          )}
+        </Chart>
       )}
-    </Chart>
+    </>
   )
 }
 
@@ -110,7 +133,6 @@ const Chart = styled(Box)`
 
     .timeline {
       display: flex;
-      justify-content: space-between;
       width: 100%;
 
       & > div:nth-child(1) {
@@ -183,4 +205,18 @@ const Chart = styled(Box)`
       }
     }
   }
+`
+
+const EmptyState = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+
+  border-radius: 4px;
+  border-style: solid;
+  border-color: #dde4e9;
+  border-width: 1px;
+  background: #ffffff;
 `
--- apps/aragon-fundraising/app/src/screens/MyOrders.js
+++ apps/aragon-fundraising/app/src/screens/MyOrders.js
@@ -20,6 +20,7 @@ import DateRangeInput from '../components/DateRange/DateRangeInput'
 import ToggleFiltersButton from '../components/ToggleFiltersButton'
 import { Order } from '../constants'
 import { round } from '../lib/math-utils'
+import EmptyOrders from '../assets/EmptyOrders.svg'
 
 const multiplyArray = (base, times) => {
   return [...Array(times)].reduce(v => [...v, ...base], [])
@@ -95,113 +96,121 @@ export default ({ orders, account, onClaim }) => {
 
   return (
     <ContentWrapper>
-      <DataView
-        page={page}
-        onPageChange={setPage}
-        fields={['Date', 'Status', 'Order Amount', 'Token Price', 'Order Type', 'Tokens', 'Actions']}
-        entries={filter(multiplyArray(filteredOrders, 10), state)}
-        mode={layoutName !== 'large' ? 'list' : 'table'}
-        heading={
-          <div>
-            {layoutName !== 'large' && (
-              <ToggleFiltersButton onClick={() => setState({ ...state, showFilters: !state.showFilters })} active={state.showFilters} />
-            )}
-            <div className={layoutName !== 'large' ? (state.showFilters ? 'filter-nav' : ' filter-nav hide') : 'filter-nav'}>
-              <div className="filter-item">
-                <DateRangeInput
-                  startDate={new Date(state.date.payload.start)}
-                  endDate={new Date(state.date.payload.end)}
-                  onChange={payload => setState({ ...state, date: { payload: { start: payload.start.getTime(), end: payload.end.getTime() } } })}
-                />
-              </div>
+      {!filteredOrders.length && (
+        <EmptyState>
+          <img src={EmptyOrders} />
+          <p css="font-size: 24px; margin-top: 1rem;">There are no orders to show.</p>
+        </EmptyState>
+      )}
+      {!!filteredOrders.length && (
+        <DataView
+          page={page}
+          onPageChange={setPage}
+          fields={['Date', 'Status', 'Order Amount', 'Token Price', 'Order Type', 'Tokens', 'Actions']}
+          entries={filter(multiplyArray(filteredOrders, 10), state)}
+          mode={layoutName !== 'large' ? 'list' : 'table'}
+          heading={
+            <div>
+              {layoutName !== 'large' && (
+                <ToggleFiltersButton onClick={() => setState({ ...state, showFilters: !state.showFilters })} active={state.showFilters} />
+              )}
+              <div className={layoutName !== 'large' ? (state.showFilters ? 'filter-nav' : ' filter-nav hide') : 'filter-nav'}>
+                <div className="filter-item">
+                  <DateRangeInput
+                    startDate={new Date(state.date.payload.start)}
+                    endDate={new Date(state.date.payload.end)}
+                    onChange={payload => setState({ ...state, date: { payload: { start: payload.start.getTime(), end: payload.end.getTime() } } })}
+                  />
+                </div>
 
-              <div className="filter-item">
-                <span className="filter-label">Token</span>
-                <DropDown
-                  items={state.token.payload}
-                  selected={state.token.active}
-                  onChange={idx => setState({ ...state, token: { ...state.token, active: idx } })}
-                />
-              </div>
-              <div className="filter-item">
-                <span className="filter-label">Order Type</span>
-                <DropDown
-                  items={state.order.payload}
-                  selected={state.order.active}
-                  onChange={idx => setState({ ...state, order: { ...state.order, active: idx } })}
-                />
-              </div>
-              <div className="filter-item">
-                <span className="filter-label">Price</span>
-                <DropDown
-                  items={state.price.payload}
-                  selected={state.price.active}
-                  onChange={idx => setState({ ...state, price: { ...state.price, active: idx } })}
-                />
+                <div className="filter-item">
+                  <span className="filter-label">Token</span>
+                  <DropDown
+                    items={state.token.payload}
+                    selected={state.token.active}
+                    onChange={idx => setState({ ...state, token: { ...state.token, active: idx } })}
+                  />
+                </div>
+                <div className="filter-item">
+                  <span className="filter-label">Order Type</span>
+                  <DropDown
+                    items={state.order.payload}
+                    selected={state.order.active}
+                    onChange={idx => setState({ ...state, order: { ...state.order, active: idx } })}
+                  />
+                </div>
+                <div className="filter-item">
+                  <span className="filter-label">Price</span>
+                  <DropDown
+                    items={state.price.payload}
+                    selected={state.price.active}
+                    onChange={idx => setState({ ...state, price: { ...state.price, active: idx } })}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        }
-        renderEntry={data => {
-          return [
-            <StyledText>{format(data.timestamp, 'MM/dd/yyyy - HH:mm:ss', { awareOfUnicodeTokens: true })}</StyledText>,
-            <div css="display: flex; align-items: center;">
-              {getIconState(data.state)}
-              <p css="margin-top: 0.25rem; margin-left: 0.25rem;">{data.state.charAt(0) + data.state.slice(1).toLowerCase()}</p>
-            </div>,
-            <p css={data.type === Order.Type.BUY ? 'font-weight: 600; color: #2CC68F;' : 'font-weight: 600;'}>
-              {data.type === Order.Type.BUY ? '+' : '-'}
-              {round(data.amount, 3) + ' '}
-              {data.symbol}
-            </p>,
-            <p css="font-weight: 600;">${round(data.price, 3)}</p>,
-            data.type === Order.Type.BUY ? (
-              <div
-                css={`
-                  display: inline-block;
-                  border-radius: 100px;
-                  background-color: rgba(204, 189, 244, 0.3);
-                  padding: 2px 2rem;
-                  text-transform: uppercase;
-                  color: #7546f2;
-                  font-size: 12px;
-                  font-weight: 700;
-                `}
-              >
-                {data.type}
-              </div>
-            ) : (
-              <div
-                css={`
-                  display: inline-block;
-                  border-radius: 100px;
-                  background-color: rgb(255, 212, 140, 0.3);
-                  padding: 2px 2rem;
-                  text-transform: uppercase;
-                  color: #f08658;
-                  font-size: 12px;
-                  font-weight: 700;
-                `}
-              >
-                {data.type}
-              </div>
-            ),
-            <p css="font-weight: 600;">{round(data.tokens, 3)}</p>,
-            data.state === Order.State.OVER ? (
-              <Button mode="strong" label="Claim" onClick={() => handleClaim(data)}>
-                Claim
-              </Button>
-            ) : null,
-          ]
-        }}
-        renderEntryActions={data => (
-          <ContextMenu>
-            <SafeLink href={'https://etherscan.io/tx/' + data.txHash} target="_blank">
-              <ContextMenuItem>View Tx on Etherscan</ContextMenuItem>
-            </SafeLink>
-          </ContextMenu>
-        )}
-      />
+          }
+          renderEntry={data => {
+            return [
+              <StyledText>{format(data.timestamp, 'MM/dd/yyyy - HH:mm:ss', { awareOfUnicodeTokens: true })}</StyledText>,
+              <div css="display: flex; align-items: center;">
+                {getIconState(data.state)}
+                <p css="margin-top: 0.25rem; margin-left: 0.25rem;">{data.state.charAt(0) + data.state.slice(1).toLowerCase()}</p>
+              </div>,
+              <p css={data.type === Order.Type.BUY ? 'font-weight: 600; color: #2CC68F;' : 'font-weight: 600;'}>
+                {data.type === Order.Type.BUY ? '+' : '-'}
+                {round(data.amount, 3) + ' '}
+                {data.symbol}
+              </p>,
+              <p css="font-weight: 600;">${round(data.price, 3)}</p>,
+              data.type === Order.Type.BUY ? (
+                <div
+                  css={`
+                    display: inline-block;
+                    border-radius: 100px;
+                    background-color: rgba(204, 189, 244, 0.3);
+                    padding: 2px 2rem;
+                    text-transform: uppercase;
+                    color: #7546f2;
+                    font-size: 12px;
+                    font-weight: 700;
+                  `}
+                >
+                  {data.type}
+                </div>
+              ) : (
+                <div
+                  css={`
+                    display: inline-block;
+                    border-radius: 100px;
+                    background-color: rgb(255, 212, 140, 0.3);
+                    padding: 2px 2rem;
+                    text-transform: uppercase;
+                    color: #f08658;
+                    font-size: 12px;
+                    font-weight: 700;
+                  `}
+                >
+                  {data.type}
+                </div>
+              ),
+              <p css="font-weight: 600;">{round(data.tokens, 3)}</p>,
+              data.state === Order.State.OVER ? (
+                <Button mode="strong" label="Claim" onClick={() => handleClaim(data)}>
+                  Claim
+                </Button>
+              ) : null,
+            ]
+          }}
+          renderEntryActions={data => (
+            <ContextMenu>
+              <SafeLink href={'https://etherscan.io/tx/' + data.txHash} target="_blank">
+                <ContextMenuItem>View Tx on Etherscan</ContextMenuItem>
+              </SafeLink>
+            </ContextMenu>
+          )}
+        />
+      )}
     </ContentWrapper>
   )
 }
@@ -259,4 +268,18 @@ const ContentWrapper = styled.div`
 
 const StyledText = styled(Text)`
   white-space: nowrap;
+`
+
+const EmptyState = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+
+  border-radius: 4px;
+  border-style: solid;
+  border-color: #dde4e9;
+  border-width: 1px;
+  background: #ffffff;
 `
