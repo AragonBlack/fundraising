ae0109cfc40957de774d84729eff1235ac725dae
deam <deamme@users.noreply.github.com>
2019-08-22 20:04:39 +0200

    Added info about rinkeby faucet

--- apps/aragon-fundraising/app/src/App.js
+++ apps/aragon-fundraising/app/src/App.js
@@ -171,7 +171,7 @@ const App = () => {
               <Info mode="warning" css="margin-top: 1.5rem;">
                 <Text>
                   This demo of Aragon Fundraising is still in the experimental phase. It's a peek into the capabilities of the final version and we are looking
-                  forward to your feedback.
+                  forward to your feedback. You might need some Rinkeby DAI or ANT which you can get by visiting the following site: https://faucet.aragon.black/
                 </Text>
                 <Text css="display: block;">Expect daily frontend updates and future smart contract updates.</Text>
               </Info>
