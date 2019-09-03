16c06b5f69c4095e4a337c3c4685facda65aedb7
xseignard <xavier.seignard@gmail.com>
2019-09-03 08:42:07 +0200

    Refactor bn-utils to accept various inputs:
    String, Number or BigNumber

--- apps/aragon-fundraising/app/src/utils/bn-utils.js
+++ apps/aragon-fundraising/app/src/utils/bn-utils.js
@@ -2,28 +2,28 @@ import BigNumber from 'bignumber.js'
 
 /**
  * Formats a big number to be a readable value
- * @param {BigNumber} value - value to format
+ * @param {String|Number|BigNumber} value - value to format
  * @param {Number} decimals - decimals of the value to format
  * @param {Number} decimalPlaces - how many decimals do we keep on the formatted value
  * @returns {String} the formatted value
  */
 export const formatBigNumber = (value, decimals, decimalPlaces = 2) => {
-  return value.shiftedBy(-decimals).toFormat(decimalPlaces)
+  return new BigNumber(value).shiftedBy(-decimals).toFormat(decimalPlaces)
 }
 
 /**
  * Converts a tap rate to its monthly rate
- * @param {BigNumber} value - value to convert
+ * @param {String|Number|BigNumber} value - value to convert
  * @param {Number} decimals - decimals of the value to convert
  * @returns {BigNumber} the converted value
  */
 export const toMonthlyAllocation = (value, decimals) => {
-  return value.times(4 * 60 * 24 * 30)
+  return new BigNumber(value).times(4 * 60 * 24 * 30)
 }
 
 /**
  * Converts a monthly rate to its tap rate (wei/block)
- * @param {BigNumber} value - value to convert
+ * @param {String|Number|BigNumber} value - value to convert
  * @param {Number} decimals - decimals of the value to convert
  * @returns {BigNumber} the converted value
  */
@@ -33,10 +33,20 @@ export const fromMonthlyAllocation = (value, decimals) => {
 
 /**
  * Converts a "human" value to it's decimal one
- * @param {BigNumber} value - value to convert
+ * @param {String|Number|BigNumber} value - value to convert
  * @param {Number} decimals - decimals of the value to convert
  * @returns {BigNumber} the converted value
  */
 export const toDecimals = (value, decimals) => {
   return new BigNumber(value).shiftedBy(decimals)
+}
+
+/**
+ * Converts a decimal value to it's "human" one
+ * @param {String|Number|BigNumber} value - value to convert
+ * @param {Number} decimals - decimals of the value to convert
+ * @returns {BigNumber} the converted value
+ */
+export const fromDecimals = (value, decimals) => {
+  return new BigNumber(value).shiftedBy(-decimals)
 }
