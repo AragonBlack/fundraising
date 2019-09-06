d6e98b1e305ed707098c82f16e4efb297032fd23
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-09-06 17:13:56 +0200

    Clean mocks

--- apps/aragon-fundraising/contracts/test/TestImports.sol
+++ apps/aragon-fundraising/contracts/test/TestImports.sol
@@ -3,12 +3,14 @@ pragma solidity 0.4.24;
 import "@aragon/os/contracts/acl/ACL.sol";
 import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/kernel/Kernel.sol";
-import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
 import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
+import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
 import "@aragon/apps-token-manager/contracts/TokenManager.sol";
 import "@aragon/test-helpers/contracts/EtherTokenConstantMock.sol";
 import "@aragon/test-helpers/contracts/TokenMock.sol";
 import "@ablack/fundraising-bancor-formula/contracts/BancorFormula.sol";
+import "@ablack/fundraising-shared-test-helpers/contracts/ForceSendETH.sol";
+
 
 // HACK to workaround truffle artifact loading on dependencies
 contract TestImports {
