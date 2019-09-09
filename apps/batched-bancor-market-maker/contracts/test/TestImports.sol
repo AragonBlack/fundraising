829d571d73203d1014f90c10a257e5569f6b903c
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-09-09 19:15:48 +0200

    Clean TestImports.sol

--- apps/batched-bancor-market-maker/contracts/test/TestImports.sol
+++ apps/batched-bancor-market-maker/contracts/test/TestImports.sol
@@ -6,9 +6,8 @@ import "@aragon/os/contracts/kernel/Kernel.sol";
 import "@aragon/apps-agent/contracts/Agent.sol";
 import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
 import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
-import "@aragon/test-helpers/contracts/EtherTokenConstantMock.sol";
 import "@aragon/test-helpers/contracts/TokenMock.sol";
-import "@ablack/fundraising-shared-test-helpers/contracts/SimpleMarketMakerController.sol";
+import "@ablack/fundraising-shared-test-helpers/contracts/AragonFundraisingControllerMock.sol";
 
 
 // HACK to workaround truffle artifact loading on dependencies
