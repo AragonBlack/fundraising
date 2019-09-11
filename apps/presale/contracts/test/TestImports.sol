efe64efa69e6ad01ff579cf29f4438114ad82ae5
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-09-11 15:06:48 +0200

    Update imports because of switch from MiniMe to ERC20 in main contract

--- apps/presale/contracts/test/TestImports.sol
+++ apps/presale/contracts/test/TestImports.sol
@@ -5,6 +5,7 @@ import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 import "@aragon/os/contracts/kernel/Kernel.sol";
 import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
+import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
 import "@ablack/fundraising-shared-test-helpers/contracts/AragonFundraisingControllerMock.sol";
 
 
