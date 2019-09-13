bdd3eccf63462cc7e48e3d5e39ba60032bcd8c36
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-09-13 19:20:22 +0200

    Update truffle config

--- templates/multisig/truffle.js
+++ templates/multisig/truffle.js
@@ -1,11 +1,19 @@
 const truffle = require('@aragon/os/truffle-config')
-truffle.solc.optimizer.runs = 3000
-truffle.networks.rpc.gas = 6.9e6
+
+const gasLimit = 7e6 - 1
+
+truffle.solc.optimizer.runs = 1
+truffle.networks.rpc.gas = gasLimit
+truffle.networks.devnet.gas = gasLimit
+truffle.networks.rinkeby.gas = gasLimit
+truffle.networks.ropsten.gas = gasLimit
+truffle.networks.kovan.gas = gasLimit
 truffle.networks.frame = {
-  host: 'localhost', // Frame
-  port: '1248', // Frame
-  network_id: 4,
-  gas: 6.9e6,
+  host: 'localhost',
+  port: '1248',
+  network_id: '*',
+  gas: gasLimit,
   gasPrice: 15000000001,
 }
+
 module.exports = truffle
