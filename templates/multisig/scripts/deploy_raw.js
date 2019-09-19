3a41d507d1906ee4cf706cea2c43965d5190b84c
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-09-19 18:04:39 +0200

    Update deployment scripts

--- templates/multisig/scripts/deploy_raw.js
+++ templates/multisig/scripts/deploy_raw.js
@@ -2,6 +2,7 @@ const { hash: namehash } = require('eth-ens-namehash')
 const deployDAOFactory = require('@aragon/os/scripts/deploy-daofactory')
 const ENS = artifacts.require('ENS')
 const MiniMeTokenFactory = artifacts.require('MiniMeTokenFactory')
+const TokenMock = artifacts.require('TokenMock')
 const FundraisingMultisigTemplate = artifacts.require('FundraisingMultisigTemplate')
 // const FundraisingMultisigTemplateHatch = artifacts.require('FundraisingMultisigTemplateHatch')
 
@@ -10,15 +11,20 @@ const aragonIDHash = namehash('aragonid.eth')
 module.exports = async callback => {
   try {
     if (process.argv[4] === 'rpc') {
+      const dai = await TokenMock.new('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', 100000e18, 'DAI', 'DAI')
+      const ant = await TokenMock.new('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', 100000e18, 'ANT', 'ANT')
       const ens = '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1'
       const owner = '0xb4124ceb3451635dacedd11767f004d8a28c6ee7'
       const ensRegistry = await ENS.at(ens)
       const aragonId = await ensRegistry.owner(aragonIDHash)
       const { daoFactory } = await deployDAOFactory(null, { artifacts: artifacts, owner, verbose: false })
       const miniMeFactory = await MiniMeTokenFactory.new()
-      const template = await FundraisingMultisigTemplate.new(daoFactory.address, ens, miniMeFactory.address, aragonId)
+      const template = await FundraisingMultisigTemplate.new(daoFactory.address, ens, miniMeFactory.address, aragonId, dai.address, ant.address)
       console.log(template.address)
     } else if (process.argv[4] === 'hatch') {
+      /**
+       * TODO
+       */
       // const ens = '0x98Df287B6C145399Aaa709692c8D308357bC085D'
       // const owner = '0xb71d2d88030a00830c3d45f84c12cc8aaf6857a5'
       // const daoFactory = '0xfdef49fbfe37704af55636bdd4b6bc8cd19143f6'
