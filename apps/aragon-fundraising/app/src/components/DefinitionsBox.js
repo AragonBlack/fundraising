de4a8acc3052638ff153c3181149dfe6452cbb64
Pierre Bertet <hello@pierre.world>
2019-09-19 16:29:49 +0100

    Reserve settings: use more components from aragonUI

---
+++ apps/aragon-fundraising/app/src/components/DefinitionsBox.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Box, GU, useTheme } from '@aragon/ui'
+
+function DefinitionsBox({ heading, definitions }) {
+  const theme = useTheme()
+  return (
+    <Box heading={heading}>
+      <ul>
+        {definitions.map(({ label, content }, index) => (
+          <li
+            key={index}
+            css={`
+              display: flex;
+              justify-content: space-between;
+              list-style: none;
+              color: ${theme.surfaceContent};
+              & + & {
+                margin-top: ${2 * GU}px;
+              }
+              > span:nth-child(1) {
+                color: ${theme.surfaceContentSecondary};
+              }
+              > span:nth-child(2) {
+                // “:” is here for accessibility reasons, we can hide it
+                opacity: 0;
+                width: 10px;
+              }
+              > span:nth-child(3) {
+                flex-shrink: 1;
+              }
+              > strong {
+                text-transform: uppercase;
+              }
+            `}
+          >
+            <span>{label}</span>
+            <span>:</span>
+            {content}
+          </li>
+        ))}
+      </ul>
+    </Box>
+  )
+}
+
+export default DefinitionsBox
