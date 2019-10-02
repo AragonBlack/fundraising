509dc2f5a0d45fac01018a2cc02587c82a3b9861
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-10-02 13:08:45 +0200

    Update to use native @aragon/ui info box

--- apps/aragon-fundraising/app/src/components/NewOrder/Info.js
+++ apps/aragon-fundraising/app/src/components/NewOrder/Info.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useAppState } from '@aragon/api-react'
+import { Info } from '@aragon/ui'
 
-const Info = ({ isBuyOrder, slippage }) => {
+const Information = ({ isBuyOrder, slippage }) => {
   // *****************************
   // background script state
   // *****************************
@@ -13,25 +14,17 @@ const Info = ({ isBuyOrder, slippage }) => {
   return (
     <div
       css={`
-        background-color: #f1fbff;
-        border-radius: 4px;
-        color: #188aaf;
-        padding: 1rem;
         margin-top: 2rem;
-        border-left: 2px solid #0ab0e5;
       `}
     >
-      <p css="font-weight: 700;">Info</p>
-      <p>
-        {isBuyOrder && 'Opening a buy order will lead you to purchase some shares in this organization.'}
-        {!isBuyOrder && 'Opening a sell order will lead you to redeem some of your shares in this organization.'}
-      </p>
-      <p>
-        Note that the return of your order may be different than the one indicated if other users open buy or sell orders simultaneously. In any case you can be
-        assured that the price slippage won't exceed <b>{slippagePct} %</b>.
-      </p>
+      <Info.Action title="Slippage">
+        <p>
+          The exact return of your order may differ from the one indicated if other users open buy or sell orders simultaneously. In any case you can be
+          assured that the price slippage won't exceed <b>{slippagePct}%</b>.
+        </p>
+      </Info.Action>
     </div>
   )
 }
 
-export default Info
+export default Information
