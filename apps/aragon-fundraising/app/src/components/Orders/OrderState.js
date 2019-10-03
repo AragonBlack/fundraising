70c00bed47c88ac771dc668180fb547d28a2a4d0
deam <deamme@users.noreply.github.com>
2019-10-03 08:24:31 +0200

    Minor UI improvements in OrderState

--- apps/aragon-fundraising/app/src/components/Orders/OrderState.js
+++ apps/aragon-fundraising/app/src/components/Orders/OrderState.js
@@ -5,16 +5,16 @@ import { Order } from '../../constants'
 export default ({ state }) => {
   let icon
   if (state === Order.state.CLAIMED) {
-    icon = <IconCheck size="small" color="#2CC68F" />
+    icon = <IconCheck size="medium" color="#2CC68F" />
   } else if (state === Order.state.OVER) {
-    icon = <IconClock size="small" color="#08BEE5" />
+    icon = <IconClock size="medium" color="#08BEE5" />
   } else if (state === Order.state.PENDING) {
-    icon = <IconEllipsis size="small" color="#6D777B" />
+    icon = <IconEllipsis size="medium" color="#6D777B" />
   }
   return (
     <>
       {icon}
-      <p css="margin-top: 0.25rem; margin-left: 0.25rem;">{state.charAt(0) + state.slice(1).toLowerCase()}</p>
+      <p css="margin-top: 0.20rem; margin-left: 0.25rem;">{state.charAt(0) + state.slice(1).toLowerCase()}</p>
     </>
   )
 }
