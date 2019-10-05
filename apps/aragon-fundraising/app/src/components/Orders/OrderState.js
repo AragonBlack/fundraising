2f9bea98a5fa44689592fcc7ec938907a626d05e
deam <deamme@users.noreply.github.com>
2019-10-05 09:03:26 +0200

    Fixed `Over` state icon

--- apps/aragon-fundraising/app/src/components/Orders/OrderState.js
+++ apps/aragon-fundraising/app/src/components/Orders/OrderState.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { IconCheck, IconClock, IconEllipsis, GU } from '@aragon/ui'
+import { IconCheck, IconEllipsis, GU } from '@aragon/ui'
 import { Order } from '../../constants'
+import overIcon from '../../assets/overIcon.svg'
 
 export default ({ state }) => {
   let icon
   if (state === Order.state.CLAIMED) {
     icon = <IconCheck size="medium" color="#2CC68F" />
   } else if (state === Order.state.OVER) {
-    icon = <IconClock size="medium" color="#08BEE5" />
+    icon = <img src={overIcon} />
   } else if (state === Order.state.PENDING) {
     icon = <IconEllipsis size="medium" color="#6D777B" />
   }
