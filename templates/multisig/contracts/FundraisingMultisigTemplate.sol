40c465fc4a0e9ab0e88ce7125eb8d2ab79600630
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2019-12-04 17:09:02 +0100

    Fix comments

--- templates/multisig/contracts/FundraisingMultisigTemplate.sol
+++ templates/multisig/contracts/FundraisingMultisigTemplate.sol
@@ -330,7 +330,7 @@ contract FundraisingMultisigTemplate is EtherTokenConstant, BaseTemplate {
         (, Voting shareVoting) = _shareAppsCache();
         (,,,, Controller controller) = _fundraisingAppsCache();
 
-        // create and grant ADD_PROTECTED_TOKEN_ROLE to this template
+        // create and grant ADD_COLLATERAL_TOKEN_ROLE to this template
         _createPermissionForTemplate(acl, address(controller), controller.ADD_COLLATERAL_TOKEN_ROLE());
         // add DAI both as a protected collateral and a tapped token
         controller.addCollateralToken(
